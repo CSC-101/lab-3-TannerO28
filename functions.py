befa201d5d4f8598c28d27fdef37d5640052cd70
tannerokeefe_first_commit <twokeefe@calpoly.edu>
2026-04-23 14:16:26 -0700

    Commit 1

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-# Double the value of a number.
+# Double the value of a number. Changed to n*2 so it actually doubles the value.
 # Input: a number to be doubled
 # Result: a number
 def double(n:int) -> int:
